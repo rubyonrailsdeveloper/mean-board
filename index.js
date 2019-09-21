c87d42edd101c9d9de7bfc46c8b681915505be56
rubyonrailsdeveloper <webdeveloper1001@outlook.com>
2019-09-21 05:57:18 +0200

    backup index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// index.js
+
+var express    = require("express");
+var mongoose   = require("mongoose");
+var bodyParser  = require("body-parser");
+var methodOverride = require("method-override");
+var app = express();
+
 // DB setting
 mongoose.connect(process.env.MONGO_DB, { useMongoClient: true });
 var db = mongoose.connection;
