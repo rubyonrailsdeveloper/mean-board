2410c62685fc3c0a90847da5d40715cdd120144e
rubyonrailsdeveloper <webdeveloper1001@outlook.com>
2019-09-22 05:55:45 +0200

    change index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// index.js
-
-var express    = require("express");
-var mongoose   = require("mongoose");
-var bodyParser  = require("body-parser");
-var methodOverride = require("method-override");
-var app = express();
-
 // DB setting
 mongoose.connect(process.env.MONGO_DB, { useMongoClient: true });
 var db = mongoose.connection;
